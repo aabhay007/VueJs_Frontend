b30c4755be8b217a65c61d5d112fb0457a5f7c23
Abhay <abhay007official@gmail.com>
2024-11-09 00:18:03 +0530

    Removed some commented code

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,12 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/home/Home.vue';
 import Login from '../views/auth/Login.vue';
 import Register from '../views/auth/Register.vue';
-// import { useAuthStore } from '../store/useAuthStore';
 import ItemsTable from '../components/Items/ItemsTable.vue';
 
 const routes = [
   { path: '/home', name: 'Home', component: Home
-    // , meta: { requiresAuth: true } 
   },
   { path: '/', name: 'Login', component: Login},
   { path: '/register', name: 'Register', component: Register },
@@ -19,14 +17,5 @@ const router = createRouter({
   history: createWebHistory(),
   routes,
 });
-
-// router.beforeEach((to, _, next) => {
-//   const authStore = useAuthStore();
-//   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-//     next('/home');
-//   } else {
-//     next();
-//   }
-// });
 
 export default router;
--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -6,18 +6,13 @@ import router from '../router';
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     user: null as { id: string; username: string } | null,
-    // token: localStorage.getItem('token') || '',
-    // isAuthenticated: !!localStorage.getItem('token'),
   }),
 
   actions: {
     async login(username: string, password: string) {
       try {
         const response = await axios.post('http://127.0.0.1:8000/api/login/', { username, password });
-        // this.token = response.data.token;
         this.user = response.data.user;
-        // this.isAuthenticated = true;
-        // localStorage.setItem('token', this.token);
       } catch (error) {
         throw new Error('Invalid credentials');
       }
@@ -36,21 +31,15 @@ export const useAuthStore = defineStore('auth', {
       try {
         const response = await axios.get('http://127.0.0.1:8000/api/user/');
         this.user = response.data;  // Store user data in state
-        // console.log(this.user);
       } catch (error) {
         console.error('Failed to fetch user:', error);
-        // this.logout();  // Logout if fetching user fails (e.g., invalid token)
       }
     },
 
     logout() {
       this.user = null;
-    
-      // Clear csrftoken cookie specifically
       document.cookie = "csrftoken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    
       router.push('/');
-      // window.location.reload();
     }
     
     
